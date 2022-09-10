d523e30d33c71b6c13679deeb7aac5e8a2f78f78
Kakshak <kakshak@gmail.com>
2022-09-09 18:30:53 -0700

    Time: 6961 ms (5.01%), Space: 13.9 MB (68.52%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -3,11 +3,9 @@ class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         
         d1 = Counter(s1)
-        k = len(s1)
         for i in range(len(s2)):  
-            sub = s2[i:i+k]  
+            sub = s2[i:i+len(s1)]
             d2 = Counter(sub)
             if d1 == d2:
                 return True
         return False
-        

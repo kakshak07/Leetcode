69ed82b4128e111f5d12d314f67b78ac03c0577c
Kakshak <kakshak@gmail.com>
2022-10-03 22:01:11 +0200

    Time: 984 ms (60.16%), Space: 24.1 MB (50.19%) - LeetHub

--- 1546-maximum-number-of-non-overlapping-subarrays-with-sum-equals-target/1546-maximum-number-of-non-overlapping-subarrays-with-sum-equals-target.py
+++ 1546-maximum-number-of-non-overlapping-subarrays-with-sum-equals-target/1546-maximum-number-of-non-overlapping-subarrays-with-sum-equals-target.py
@@ -1,12 +1,16 @@
 class Solution:
     def maxNonOverlapping(self, nums: List[int], target: int) -> int:
-        f={}
-        f[0]=1;s=0;ans=0
-        for j in nums:
-            s+=j
-            if s-target in f:
-                ans+=1
-                f={}
-            f[s]=1
-        return ans
         
+        f = {}
+        s = 0
+        count = 0
+        f[0] = 1
+        for i in nums:
+            s += i
+            if s-target in f:
+                count += 1
+                f = {}
+            f[s] = 1
+        return(count)
+            
+            

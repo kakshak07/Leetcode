217854e16872e52fce7280c26aa2ffc3ab7a7973
Kakshak <kakshak@gmail.com>
2022-10-30 18:33:45 -0700

    Time: 1781 ms (76.14%), Space: 35.8 MB (9.71%) - LeetHub

---
+++ 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def findLength(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: int
+        """
+        A = nums1
+        B = nums2
+        dp = [[0 for _ in range(len(B) + 1)] for _ in range(len(A) + 1)]
+        for i in range(1, len(A) + 1):
+            for j in range(1, len(B) + 1):
+                if A[i - 1] == B[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1] + 1
+        return max(max(row) for row in dp)

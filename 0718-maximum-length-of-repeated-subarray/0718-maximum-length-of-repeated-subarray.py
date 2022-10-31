146231d88bb65f6db5632c19cc013479992f08f4
Kakshak <kakshak@gmail.com>
2022-10-30 18:36:00 -0700

    Time: 2004 ms (74.42%), Space: 35.9 MB (5.06%) - LeetHub

--- 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.py
+++ 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.py
@@ -12,4 +12,7 @@ class Solution(object):
             for j in range(1, len(B) + 1):
                 if A[i - 1] == B[j - 1]:
                     dp[i][j] = dp[i - 1][j - 1] + 1
+                # else:
+                    # dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+                    
         return max(max(row) for row in dp)
